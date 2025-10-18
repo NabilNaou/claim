25fc363434d4a502c6cdf8cc498b4e914cf5a67d
NabilNaou <nabilnaou@hotmail.com>
2025-10-18 16:16:42 +0200

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,36 @@ import "./globals.css";
 
 export const metadata = {
   title: "ClaimFlow",
-  description: "Claim wizard demo",
+  description: "Claimwizard built using Next/React - demo & practice.",
+  keywords: ["claimflow", "next.js", "typescript", "frontend", "react"],
+  authors: [{ name: "Nabil Naou", url: "https://github.com/nabilnaou" }],
+
+  // Favicon. TODO: Find alternative to generated favicon.io
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/android-chrome-192x192.png", sizes: "192x192" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+
+  // General social media cards (slack, discord etc)
+  openGraph: {
+    title: "ClaimFlow — ClaimWizard",
+    description: "ClaimFlow Demo & Practice.",
+    siteName: "ClaimFlow",
+    locale: "nl_NL",
+    type: "website",
+  },
+
+  // Twitter cards
+  twitter: {
+    title: "ClaimFlow — Klantvriendelijke Claimwizard",
+    description: "Claim wizard",
+    creator: "@nabilnaou",
+  },
 };
 
 // WCAG: First tab allows screenreaders to skip to the main content, if disabled person desires.
